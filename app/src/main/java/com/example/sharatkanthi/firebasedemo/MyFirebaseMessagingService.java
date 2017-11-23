2dcdc55e8ab6f444b512bf20d672a4cfcd6b5953
coderekadabra <sharat.kanthi@gmail.com>
2017-11-23 18:08:51 +0530

    no logs here

--- app/src/main/java/com/example/sharatkanthi/firebasedemo/MyFirebaseMessagingService.java
+++ app/src/main/java/com/example/sharatkanthi/firebasedemo/MyFirebaseMessagingService.java
@@ -24,8 +24,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService
 
         PendingIntent pendingIntent  =  PendingIntent.getActivity(this,0,intent,PendingIntent.FLAG_ONE_SHOT);
 
-        Log.d("Showing Message->","Message received : " + remoteMessage.getData());
-
         NotificationCompat.Builder notificationBuilder  = new NotificationCompat.Builder(this);
                 notificationBuilder.setContentTitle("FCM NOTIFICATION");
                 notificationBuilder.setContentText(remoteMessage.getNotification().getBody());
